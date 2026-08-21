0839f8e8ac1a8f0cde257cb9baa08244a4f4875a
Roy Nadiel <almadaroy19@gmail.com>
2026-08-21 13:02:15 -0400

    style: apply Vercel and VenDevs light design tokens and typography

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,8 @@
 export default function Layout({ children }) {
   return (
-    <div className="relative z-10 max-w-[1600px] mx-auto flex flex-col items-center">
+    <div className="relative z-10 w-full min-h-screen flex flex-col items-center overflow-x-hidden">
       {children}
     </div>
   );
 }
+
